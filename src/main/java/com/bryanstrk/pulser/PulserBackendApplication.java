57bfbfe28026cac4b9f9aacfae0a416f7b29a8eb
BryanStrk <126487933+BryanStrk@users.noreply.github.com>
2026-06-30 13:18:13 +0200

    chore: initial project setup with Spring Boot 4 and dependencies

---
+++ src/main/java/com/bryanstrk/pulser/PulserBackendApplication.java
@@ -0,0 +1,13 @@
+package com.bryanstrk.pulser;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class PulserBackendApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(PulserBackendApplication.class, args);
+    }
+
+}
---
+++ src/test/java/com/bryanstrk/pulser/PulserBackendApplicationTests.java
@@ -0,0 +1,13 @@
+package com.bryanstrk.pulser;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class PulserBackendApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
